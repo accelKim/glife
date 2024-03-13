9b139d79d760cff86179941ca3201c6e8dddccce
accelKim <dnjsgh567877@gmail.com>
2024-03-13 20:02:52 +0900

    v0.3

--- js/common.js
+++ js/common.js
@@ -7,3 +7,20 @@ var swiper = new Swiper('.mySwiper', {
         },
     },
 });
+var swiper = new Swiper(".mySwiper2", {
+    slidesPerView: 5,
+    spaceBetween: 100,
+    loop: true,
+    centeredSlides: true,
+    on: {
+        init: function () {
+          this.slides[this.activeIndex].style.transform = 'scaleY(1.1) translateY(4%)';
+        },
+        slideChange: function () {
+          this.slides.forEach((slide) => {
+            slide.style.transform = 'scaleX(1)';
+          });
+          this.slides[this.activeIndex].style.transform = 'scaleY(1.1) translateY(4%)';
+        }
+      }
+  });
